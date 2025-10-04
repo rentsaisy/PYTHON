74a1d1d8337385275317bf610b208f997104425b
Currents <currentsaisyah@gmail.com>
2025-10-05 05:41:03 +0700

    fourth

---
+++ Kuliah/Week 04 Alpro(1).py
@@ -0,0 +1,10 @@
+## 10 STUDY CASE
+# 1. Determine if a number is positive, negative, or zero
+print("=======Program to determine if a number is positive, negative, or zero=======")
+number = int(input("Input a number: "))
+if number > 0:
+    print("The number is positive.")
+elif number < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
