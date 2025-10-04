2fcbcea191d77b58d530f11b7172e222471f179e
Currents <currentsaisyah@gmail.com>
2025-10-05 05:45:03 +0700

    fifth

--- Kuliah/Week 04 Alpro(1).py
+++ Kuliah/Week 04 Alpro(1).py
@@ -8,3 +8,19 @@ elif number < 0:
     print("The number is negative.")
 else:
     print("The number is zero.")
+
+# 2. Determine the grade based on the score
+print("=======Program to determine the grade based on the score=======")
+score = int(input("Input the score (0-100): "))
+if score >= 85 and score <= 100:
+    print("Grade: A")
+elif score >= 70 and score <= 84:
+    print("Grade: B")
+elif score >= 55 and score <= 69:
+    print("Grade: C")
+elif score >= 40 and score <= 54:
+    print("Grade: D")
+elif score >= 0 and score <= 39:
+    print("Grade: E")
+else:
+    print("Invalid score input.")
