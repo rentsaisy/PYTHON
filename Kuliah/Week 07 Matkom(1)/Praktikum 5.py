0bde9ab63a4411440865b6c4b4c0e76665ae15cd
Currents <currentsaisyah@gmail.com>
2025-10-13 23:10:50 +0700

    dem

--- Kuliah/Week 07 Matkom(1)/Praktikum 5.py
+++ Kuliah/Week 07 Matkom(1)/Praktikum 5.py
@@ -1,19 +1,19 @@
-# set_a = {1, 2, 3}
-# set_b = {2, 3, 4}
-# set_c = {3, 4, 5}
+set_a = {1, 2, 3}
+set_b = {2, 3, 4}
+set_c = {3, 4, 5}
 
-# # Operasi gabungan
-# union_set = set_a.union(set_b, set_c)
+# Operasi gabungan
+union_set = set_a.union(set_b, set_c)
 
-# # Operasi irisan
-# intersection_set = set_a.intersection(set_b, set_c)
+# Operasi irisan
+intersection_set = set_a.intersection(set_b, set_c)
 
-# # Operasi selisih
-# difference_set = set_a.difference(set_b.union(set_c))
+# Operasi selisih
+difference_set = set_a.difference(set_b.union(set_c))
 
-# print(f"Gabungan: {union_set}")
-# print(f"Irisan: {intersection_set}")
-# print(f"Selisih: {difference_set}")
+print(f"Gabungan: {union_set}")
+print(f"Irisan: {intersection_set}")
+print(f"Selisih: {difference_set}")
 
 set_a = {1, 2}
 set_b = {2, 3}
