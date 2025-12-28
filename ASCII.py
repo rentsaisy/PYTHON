e650fb330be097105fa6e9b0bd51e1374083aa9f
Currents <currentsaisyah@gmail.com>
2025-12-29 06:54:16 +0700

    exercise 7

---
+++ ASCII.py
@@ -0,0 +1,7 @@
+def printASCIITable(): 
+    # Loop over integers 32 up to and including 126: 
+    for i in range(32, 126): 
+        # Print the integer and its ASCII text character: 
+        print(i, chr(i)) 
+
+printASCIITable()
