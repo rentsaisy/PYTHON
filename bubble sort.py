9ad7cdcd3600a93e67e9390e85703d96312ca1c8
Currents <currentsaisyah@gmail.com>
2026-01-20 19:44:27 +0700

    try to make change

--- bubble sort.py
+++ bubble sort.py
@@ -1,10 +1,10 @@
 def bubbleSort(numbers): 
     # The outer loop loops i over all but the last number: 
-    for i in range(len(____) - ____): 
+    for i in range(len(numbers) - 1): 
         # The inner loop loops j starting at i to the last number: 
-        for j in range(____, len(____)): 
+        for j in range(i + 1, len(numbers)): 
             # If the number at i is greater than the number at j, swap them: 
-            if numbers[i] ____ numbers[j]: 
-                numbers[i], numbers[j] = numbers[____], numbers[____] 
+            if numbers[i] > numbers[j]: 
+                numbers[i], numbers[j] = numbers[j], numbers[i] 
     # Return the now-sorted list: 
-    return numbers 
+    return numbers
